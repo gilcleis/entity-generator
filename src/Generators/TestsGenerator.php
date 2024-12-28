81eedc1268c2549b580ec619ba0a953683d8eab7
Gilclei Araujo <gilclei.araujo@saude.ba.gov.br>
2024-12-28 10:02:23 -0300

    refactor(TestsGenerator): improve test generation process
    - Restructure the `generate` method to use a loop for different test types
    - Extract validation methods for API test and controller existence
    - Create separate methods for generating each test type
    - Improve code reusability and maintainability

--- src/Generators/TestsGenerator.php
+++ src/Generators/TestsGenerator.php
@@ -4,23 +4,38 @@ namespace Gilcleis\Support\Generators;
 
 use Gilcleis\Support\Events\SuccessCreateMessage;
 use Gilcleis\Support\Exceptions\ClassNotExistsException;
+use Illuminate\Support\Str;
 
 class TestsGenerator extends EntityGenerator
 {
-    public function getTestClassName(): string
-    {
-        return "{$this->model}Test";
-    }
-
     protected bool $withAuth = false;
+    protected array $testTypes = ['repository', 'model', 'service', 'api'];
+
+    // public function getTestClassName(): string
+    // {
+    //     return "{$this->model}Test";
+    // }
 
     public function generate(): void
     {
+        $this->validateApiTestExistence();
+        $this->validateControllerExistence();
+
+        foreach ($this->testTypes as $testType) {
+            $this->generateTest($testType);
+        }
+    }
+
+    protected function validateApiTestExistence(): void
+    {
         if ($this->classExists('tests_apis', "{$this->model}ApiTest")) {
             event(new SuccessCreateMessage("Cannot create {$this->model} ApiTest cause {$this->model}ApiTest already exists."));
-
             return;
         }
+    }
+
+    protected function validateControllerExistence(): void
+    {
         if (!$this->classExists('controllers', "{$this->model}Controller")) {
             $this->throwFailureException(
                 ClassNotExistsException::class,
@@ -28,42 +43,22 @@ class TestsGenerator extends EntityGenerator
                 "Create a {$this->model} Controller by himself or run command 'php artisan make:entity {$this->model} --only-controller'."
             );
         }
+    }
 
-        $content = $this->getStub('test_repository', [
-            'entity' => $this->model,
-            'databaseTableName' => $this->getTableName($this->model),
-            'entities' => $this->getTableName($this->model, '-'),
-            'withAuth' => $this->withAuth,
-            'modelsNamespace' => $this->getOrCreateNamespace('models'),
-            'fields' => $this->prepareFields()
+    protected function generateTest(string $testType): void
+    {
+        $content = $this->getTestContent($testType);
+        $className = $this->getTestClassName($testType);
+        $folderName = $this->getTestFolderName($testType);
 
-        ]);
+        $this->saveClass($folderName, $className, $content);
 
-        $testName = $this->getTestClassName();
-        $createMessage = "Created a new Test: {$this->model}RepositoryTest";
+        event(new SuccessCreateMessage("Created a new Test: {$className}"));
+    }
 
-        $this->saveClass('tests_repositories', "{$this->model}RepositoryTest", $content);
-
-        event(new SuccessCreateMessage($createMessage));
-
-        $content = $this->getStub('test_model', [
-            'entity' => $this->model,
-            'databaseTableName' => $this->getTableName($this->model),
-            'entities' => $this->getTableName($this->model, '-'),
-            'withAuth' => $this->withAuth,
-            'modelsNamespace' => $this->getOrCreateNamespace('models'),
-            'casts' => $this->getCasts($this->fields),
-            'fields' => $this->prepareFields()
-
-        ]);
-
-        $createMessage = "Created a new Test: {$this->model}ModelTest";
-
-        $this->saveClass('tests_models', "{$this->model}ModelTest", $content);
-
-        event(new SuccessCreateMessage($createMessage));
-
-        $content = $this->getStub('test_service', [
+    protected function getTestContent(string $testType): string
+    {
+        return $this->getStub("test_{$testType}", [
             'entity' => $this->model,
             'databaseTableName' => $this->getTableName($this->model),
             'entities' => $this->getTableName($this->model, '-'),
@@ -71,31 +66,17 @@ class TestsGenerator extends EntityGenerator
             'modelsNamespace' => $this->getOrCreateNamespace('models'),
             'casts' => $this->getCasts($this->fields),
             'fields' => $this->prepareFields()
-
         ]);
+    }
 
-        $createMessage = "Created a new Test: {$this->model}ServiceTest";
+    protected function getTestClassName(string $testType): string
+    {
+        return "{$this->model}" . ucfirst($testType) . "Test";
+    }
 
-        $this->saveClass('tests_services', "{$this->model}ServiceTest", $content);
-
-        event(new SuccessCreateMessage($createMessage));
-
-        $content = $this->getStub('test_api', [
-            'entity' => $this->model,
-            'databaseTableName' => $this->getTableName($this->model),
-            'entities' => $this->getTableName($this->model, '-'),
-            'withAuth' => $this->withAuth,
-            'modelsNamespace' => $this->getOrCreateNamespace('models'),
-            'casts' => $this->getCasts($this->fields),
-            'fields' => $this->prepareFields()
-
-        ]);
-
-        $createMessage = "Created a new Test: {$this->model}ApiTest";
-
-        $this->saveClass('tests_apis', "{$this->model}ApiTest", $content);
-
-        event(new SuccessCreateMessage($createMessage));
+    protected function getTestFolderName(string $testType): string
+    {
+        return "tests_".Str::plural($testType) ;
     }
 
     protected function prepareFields(): array
